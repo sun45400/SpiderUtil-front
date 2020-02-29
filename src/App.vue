feeef124ea7850bcee484af8a558761863796972
SunBangchao <>
2020-02-29 10:37:49 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/spider.png">
-    <HelloWorld msg="Welcome to Spider Util. This is create by Vue.js App"/>
+    <HelloWorld msg="Welcome to Spider Util."/>
   </div>
 </template>
 
